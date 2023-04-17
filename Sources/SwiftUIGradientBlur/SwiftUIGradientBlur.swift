e9c740cbc08cd794445d166097178ee33fec9240
unitedadi <87424096+unitedadi@users.noreply.github.com>
2023-04-17 13:16:49 +0400

    Added Video

--- Sources/SwiftUIGradientBlur/SwiftUIGradientBlur.swift
+++ Sources/SwiftUIGradientBlur/SwiftUIGradientBlur.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import AVKit
 
 struct GradientImageView : View {
     var image: Image
@@ -12,6 +13,7 @@ struct GradientImageView : View {
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: width , height: height)
+                .clipped()
             
             
             VStack{}
@@ -34,6 +36,37 @@ struct GradientImageView : View {
     }
 }
 
+struct GradientVideoView : View {
+    let player: AVPlayer
+    var height: CGFloat
+    var width: CGFloat
+    var style : UIBlurEffect.Style = .dark
+    
+    var body: some View {
+        ZStack(alignment: .bottom){
+            VideoPlayerView(player: player)
+                .frame(width: width , height: height)
+                .clipped()
+            
+            
+            VStack{}
+                .frame(width: width , height: height)
+                .background(
+                    VisualEffectView(effect: UIBlurEffect(style: style))
+                )
+            
+            
+            VideoPlayerView(player: player)
+                .frame(width: width , height: height)
+                .mask(LinearGradient(stops: [.init(color: .white, location: 0),
+                                             .init(color: .white, location: 0.4),
+                                             .init(color: .clear, location: 0.80),], startPoint: .top, endPoint: .bottom))
+
+        }
+        
+    }
+}
+
 struct VisualEffectView: UIViewRepresentable {
     var effect: UIVisualEffect?
     
@@ -46,3 +79,18 @@ struct VisualEffectView: UIViewRepresentable {
     }
 }
 
+
+struct VideoPlayerView: UIViewControllerRepresentable {
+    let player: AVPlayer
+    
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let view = AVPlayerViewController()
+        view.player = player
+        view.showsPlaybackControls = false
+        view.videoGravity = .resizeAspectFill
+        
+        return view
+    }
+    
+    func updateUIViewController(_ uiView: AVPlayerViewController, context: Context) {}
+}
